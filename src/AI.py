4d9ec71c17da75344ad5e55a1233846a1883d672
anmiimna <aminzolfagharian3@gmail.com>
2022-08-05 19:04:51 +0430

    simp added

--- src/AI.py
+++ src/AI.py
@@ -1,22 +1,11 @@
 from src.client import GameClient
 from src.model import GameView
-from src.memory import memory as Memory
 from src import game as Game
-from src import tools
-
-log = tools.Logger()
-mem = Memory.Memory()
-
-
-
 
 
 def get_thief_starting_node(view: GameView) -> int:
     # write your code here
-    log.setup(view.viewer)
     game = Game(view)
-    mem.update(game)
-
     return 2
 
 
@@ -34,21 +23,17 @@ class AI:
 
     def thief_move_ai(self, view: GameView) -> int:
         # write your code here
-        log.setup(view.viewer)
         game = Game(view)
-        mem.update(game)
 
         message = ''
         for m in range(len(view.visible_agents)):
-            message = message  + '0'
+            message = message + '0'
         self.phone.send_message(message)
         return 2
 
     def police_move_ai(self, view: GameView) -> int:
         # write your code here
-        log.setup(view.viewer)
         game = Game(view)
-        mem.update(game)
 
         self.phone.send_message('00101001')
         return 1
